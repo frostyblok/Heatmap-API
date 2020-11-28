c9c41cc96da827926c92d234977bf6589c27dbca
Oluwakunle Fakorede <fakoredeoluwakunle2626@gmail.com>
2020-11-28 19:30:27 +0100

    Genereate heatmap data

--- app/controllers/heatmap_controller.rb
+++ app/controllers/heatmap_controller.rb
@@ -1,5 +1,37 @@
 class HeatmapController < ApplicationController
   def index
-    render plain: 'hello'
+    render json: employees_heat_map
+  end
+
+  private
+
+  def employees_heat_map
+    heat_map = []
+    transformed_employees_happiness_data = transform_employees_happiness
+
+    transformed_employees_happiness_data.each do |key, _|
+      heat_map.push({driver: key.to_s, score: transformed_employees_happiness_data[key.to_s]})
+    end
+
+    heat_map
+  end
+
+  def transform_employees_happiness
+    employees_happiness = Hash.new { |h,k| h[k] = [] }
+
+    employees.each do |employee|
+      employees_happiness[employee[0]] += [[employee[1], employee[2]]]
+    end
+
+    employees_happiness.transform_values do |value|
+      value.group_by {|x| x[0]}.transform_values do |e|
+        all_scores = e.map { |k| k[1].to_i }
+        (all_scores.sum/(all_scores.size + 0.0)).round(2)
+      end
+    end
+  end
+
+  def employees
+    @employees ||= Employee.all_employees_driver_names_with_department_and_scores
   end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,7 @@
 class Employee < ApplicationRecord
+  has_many :responses
+
+  scope :all_employees_driver_names_with_department_and_scores, -> {
+    includes(:responses).where.not(responses: { id: nil }).pluck(:driver_name, :department, :score)
+  }
 end
